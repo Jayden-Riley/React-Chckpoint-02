b6b2be6b6db5b077da7afc97e38d1cbb073ae10e
Jayden Omollo <Jaydendejong83@gmail.com>
2024-11-30 15:36:21 +0300

    Initial commit from create-remix

---
+++ app/routes/_index.jsx
@@ -0,0 +1,13 @@
+export const meta = () => {
+  return [
+    { title: "FIFA Player Card" },
+    {
+      name: "description",
+      content: "Welcome to the Official FIFA Player Cars!",
+    },
+  ];
+};
+
+export default function Index() {
+  return;
+}
